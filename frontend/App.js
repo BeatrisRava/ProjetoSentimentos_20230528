5bc43166353f01cf009a1741a1d26ba69abb26b9
Beatris <ravagnanibeatris@gmail.com>
2023-05-28 15:53:22 -0300

    Entrega Sentimentos

--- frontend/App.js
+++ frontend/App.js
@@ -1,25 +1,21 @@
-//importa módulos
+
 import { useState } from 'react';
 import { StyleSheet, View} from 'react-native';
 import { Button, Input, ListItem } from '@rneui/themed';
 import axios from 'axios';
 
 export default function App() {
-  //Define um estado para a variável 'sentimento' 'listaValorInputESentimentos' 'listaSentimentos' 
-  //e uma função para atualizá-la
+
   const [valorInput, setvalorInput] = useState('');
   const [listaValorInputESentimentos, setlistaValorInputESentimentos] = useState([]);
   const [listaSentimentos, setlistaSentimentos] = useState([]);
 
-  //Atualiza o valor do estado 'mostreValorInput' com o valor digitado pelo usuário
-  //Isso fará com que o valor digitado pelo usuário apareça dentro do input.
+  
   const mostreValorInput = (atualizaValorDigitado) => {
     setvalorInput(atualizaValorDigitado);
   };
 
-  /*A função manipularValorInput verifica se o o valor do input não está vazio, 
-  adiciona a frase à lista de sentimentos e chama uma função para obter informações 
-  adicionais sobre o sentimento.*/
+  
   const manipularValorInput = () => {
     if (valorInput.trim() !== '') {
       setlistaValorInputESentimentos((listaValorInputESentimentos) => {
@@ -27,17 +23,12 @@ export default function App() {
         setvalorInput('');
         return aux;
       });
-      // Chama a função para obter o sentimento ao adicionar um sentimento
+      
       obterSentimento(valorInput); 
     }
   };
 
-/*Essa função remove um elemento da lista de sentimentos. 
-  A função removerSentimentoEFrase atualiza o estado da lista de sentimentos utilizando o hook 
-  setlistaValorInputESentimentos. Ela faz uma cópia da lista atual usando o spread operator 
-  [...listaValorInputESentimentos] e, em seguida, remove um elemento da posição indicada pelo índice 
-  utilizando o método splice. A lista atualizada é retornada e substitui a lista original 
-  no estado da aplicação.*/
+
   const removerSentimentoEFrase = (index) => {
     setlistaValorInputESentimentos((listaValorInputESentimentos) => {
       const auxFrases = [...listaValorInputESentimentos];
@@ -52,8 +43,7 @@ export default function App() {
     });
   };
 
-  //Pega somente a palavra 'Positivo', 'Negativo' ou 'Neutro' da frase retornada pela API.
-  //Ou seja, tira os '\n\n' e outros valores.
+  
   const encontrePalavra = (frase) => {
     let sentimento = '';
 
@@ -68,12 +58,12 @@ export default function App() {
   };
 
   const obterSentimento = (texto) => {
-    axios //Faz uma requisição POST para 'http://localhost:4000/sentimentos'
+    axios 
       .post('http://localhost:4000/sentimentos', { texto })
       .then((response) => {
-        //Obtém o sentimento do backend a partir da resposta da requisição.
+        
         const sentimentoDoBackend = response.data.sentimento;
-        //Atualiza o valor do estado 'sentimentos' com o novo sentimento obtido.
+        
         setlistaSentimentos((listaSentimentos) => [encontrePalavra(sentimentoDoBackend), ...listaSentimentos]);
       })
       .catch((error) => {
@@ -82,11 +72,10 @@ export default function App() {
   };
 
 
-  //Seleciona apenas os 3 primeiros listaValorInputESentimentos para serem exibidos.
+  
   const ValoresInputESentimentosExibidos = listaValorInputESentimentos.slice(0, 3);
 
-  //Este bloco de código retorna a estrutura visual da interface do aplicativo.
-  //Ex: Input, Button e ListItem.
+  
   return (
     <View style={styles.container}>
       <View style={styles.entradaView}>
@@ -109,8 +98,7 @@ export default function App() {
         {ValoresInputESentimentosExibidos.map((valorInput, index) => (
           <View key={index}>
             <ListItem
-              //Quando o usuário clicar e segurar em cima do item (frase+sentimento) 
-              //ele será removido da tela e lista.
+              
               onLongPress={() => removerSentimentoEFrase(index)}
               style={styles.listItem}
             >
@@ -131,7 +119,7 @@ export default function App() {
   );
 }
 
-//cria um objeto com estilos
+
 const styles = StyleSheet.create({
   container: {
     padding: 40,
